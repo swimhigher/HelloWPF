89ce0a6359ac91cab6ba9d97d8b7d0076b15d2ac
zhouchaoyang <endlessfantasty@outlook.com>
2021-05-26 18:04:28 +0800

    添加CM 和 Handy Control的引用

---
+++ src/Zcy/Zcy/Bootstrapper.cs
@@ -0,0 +1,58 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Zcy;
+using Zcy.View;
+
+namespace Zcy
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+        private SimpleContainer container;
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+        protected override void Configure()
+        {
+            container = new SimpleContainer();
+
+            container.Singleton<IWindowManager, WindowManager>();
+            container.Singleton<IEventAggregator, EventAggregator>();
+
+            container.PerRequest<ShellViewModel>();
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            container.BuildUp(instance);
+        }
+
+
+
+        protected override IEnumerable<Assembly> SelectAssemblies()
+        {
+            return new[] { Assembly.GetExecutingAssembly() };
+        }
+    }
+}
---
+++ src/Zcy/Zcy/ViewModel/ShellViewModel.cs
@@ -0,0 +1,46 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Zcy.View
+{
+    public class ShellViewModel : PropertyChangedBase
+    {
+        private readonly IWindowManager _windowManager;
+        string name;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                NotifyOfPropertyChange(() => Name);
+                NotifyOfPropertyChange(() => CanSayHello);
+            }
+        }
+
+        public bool CanSayHello
+        {
+            get { return !string.IsNullOrWhiteSpace(Name); }
+        }
+
+        public void SayHello()
+        {
+            MessageBox.Show(string.Format("Hello {0}!", Name)); //Don't do this in real life :)
+        }
+
+        public void Border_MouseDown(MouseButtonEventArgs e, object view)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                (view as Window).DragMove();
+            }
+        }
+    }
+}
