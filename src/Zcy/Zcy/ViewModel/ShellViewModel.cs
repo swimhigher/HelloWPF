5c6c1f3c7819f98f290775da8f60bad2e3324f10
zhouchaoyang <endlessfantasty@outlook.com>
2021-05-28 17:59:24 +0800

    win窗体调整

--- src/Zcy/Zcy/ViewModel/ShellViewModel.cs
+++ src/Zcy/Zcy/ViewModel/ShellViewModel.cs
@@ -5,13 +5,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
+
 
 namespace Zcy.View
 {
-    public class ShellViewModel : PropertyChangedBase
+    public class ShellViewModel : ViewAware
     {
+        public Button WinMaxBtn;
+        public Button WinRestoreBtn;
+
+        protected override void OnViewAttached(object view, object context)
+        {
+            base.OnViewAttached(view, context);
+
+            var frameworkElement = view as FrameworkElement;
+
+            if (frameworkElement == null)
+            {
+                return;
+            }
+
+            WinMaxBtn = frameworkElement.FindName("WinMaxBtn") as Button;
+            WinRestoreBtn = frameworkElement.FindName("WinRestoreBtn") as Button;
+        }
+
         private readonly IWindowManager _windowManager;
+
+
+        public ShellViewModel(IWindowManager windowManager)
+        {
+            _windowManager = windowManager;
+        }
         string name;
 
         public string Name
@@ -24,6 +50,7 @@ namespace Zcy.View
                 NotifyOfPropertyChange(() => CanSayHello);
             }
         }
+
 
         public bool CanSayHello
         {
@@ -42,5 +69,41 @@ namespace Zcy.View
                 (view as Window).DragMove();
             }
         }
+        public void WinClose()
+        {
+            Environment.Exit(Environment.ExitCode);
+        }
+        Rect rcnormal;
+        public void WinMax(object view)
+        {
+            WinMaxBtn.Visibility = Visibility.Collapsed;
+            WinRestoreBtn.Visibility = Visibility.Visible;
+            var window = (view as Window);
+            rcnormal = new Rect(window.Left, window.Top, window.Width, window.Height);//保存下当前位置与大小
+            window.Left = 0;//设置位置
+            window.Top = 0;
+            Rect rc = SystemParameters.WorkArea;//获取工作区大小
+            window.Width = rc.Width;
+            window.Height = rc.Height;
+        }
+        public void WinRestore(object view)
+        {
+            WinMaxBtn.Visibility = Visibility.Visible;
+            WinRestoreBtn.Visibility = Visibility.Collapsed;
+
+            var window = (view as Window);
+            window.Left = rcnormal.Left;
+            window.Top = rcnormal.Top;
+            window.Width = rcnormal.Width;
+            window.Height = rcnormal.Height;
+
+        }
+        public void WinMin(object view)
+        {
+            var window = (view as Window);
+
+            window.WindowState = System.Windows.WindowState.Minimized;
+        }
+
     }
 }
